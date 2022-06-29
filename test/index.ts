e0b127e8f41dc6f307b6d8f8c1c62e74454bd457
sorxcode <sorxcode@gmail.com>
2022-06-29 13:47:54 +0100

    fail

--- test/index.ts
+++ test/index.ts
@@ -1,9 +1,11 @@
 import { expect } from "chai";
+import { assert } from "console";
 import { ethers } from "hardhat";
 
 describe("Greeter", function () {
   it("Should log name", async function () {
     const name = "unknown";
     console.log(name);
+    expect(name).to.not.equal("unknown");
   });
 });
